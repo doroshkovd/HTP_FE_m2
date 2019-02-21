23f1a5017f39c0216b0e156ad75c868fcb5a8d97
Dzmitry Darashkou <doroshkovd@yandex.ru>
2019-02-22 00:02:26 +0300

    feat(lesson 3): add code with error for debug

---
+++ lesson_3/debug.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/**
+ * Create multiply table
+ *
+ * @returns {Array}
+ */
+function createMultiplyTable() {
+  var result = [];
+  var tdInner;
+  for (var i = 1; i <= 10; i++) {
+    for(var j = 1; j <= 10; j++) {
+      tdInner += '<div style="padding: 5px">' + i + '*' + j + '=' + (i * j) + '</div>';
+    }
+    result.push(tdInner);
+  }
+  return result;
+}
+
+/**
+ * Render multiply table
+ *
+ * @param source
+ */
+function renderMultiplyTable(source) {
+  var body = document.querySelector('body');
+  var table = document.createElement('table');
+  var tr, td;
+
+  table.style.width = '100%';
+  table.setAttribute('border', 1);
+
+  for (var i = 1; i <= source.length; i++) {
+    td = document.createElement('td');
+    if (i == 1 || i == 6) {
+      tr = document.createElement('tr');
+    }
+
+    td.innerHTML = source[i];
+    tr.appendChild(td);
+
+    if (i == 5 || i == 10) {
+      table.appendChild(tr);
+    }
+  }
+  body.appendChild(table);
+}
+
+
+
