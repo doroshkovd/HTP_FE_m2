be01cec89dbf87d555ea1d3a7690e7b5b7a076c4
Dzmitry Darashkou <doroshkovd@yandex.ru>
2019-03-12 19:46:31 +0300

    feat(lesson_8): add source for 8 lesson #1

---
+++ lesson_8/js/define-prop.js
@@ -0,0 +1,38 @@
+const obj = {
+  _prop1: 23,
+  get prop1(){
+    return this._prop1;
+  },
+  set prop1(value){
+    console.log('prop1 was changed');
+    this._prop1 = value;
+  }
+};
+
+console.log(obj.prop1);
+obj.prop1 = 56;
+console.log(obj.prop1);
+
+
+const obj2 = {
+  _prop2: 0,
+}
+
+Object.defineProperty(obj2, 'prop2', {
+  get: function() {
+    return `Значение prop2: ${this._prop2}`;
+  },
+  set: function(value) {
+    if (value < 18) {
+      console.log('Access forbiden');
+      return false;
+    }
+    this._prop2 = value;
+  }
+});
+
+console.log(obj2.prop2);
+obj2.prop2 = 12;
+obj2.prop2 = 18;
+console.log(obj2.prop2);
+
