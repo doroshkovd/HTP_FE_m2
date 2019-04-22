b32590f427454419d2062b3af8f90dccab36a7bb
darashkoud <ddarashkou@exadel.com>
2019-04-22 17:06:24 +0300

    feat(lesson-19): add changes

--- lesson_19/clock.js
+++ lesson_19/clock.js
@@ -57,19 +57,21 @@ function drawAnimationPart() {
   let second = now.getSeconds();
   //hour
   hour = hour % 12;
-  hour = ( hour * Math.PI / 6)+
-    ( minute * Math.PI / ( 6*60 ))+
-    ( second * Math.PI / (360*60));
+  hour = ( hour * Math.PI / 6) +
+    ( minute * Math.PI / ( 6 * 60 )) +
+    ( second * Math.PI / (360 * 60));
   drawHand(clockCtx, hour, radius * 0.5, radius * 0.07);
   //minute
-  minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
-  drawHand(clockCtx, minute, radius*0.8, radius*0.07);
+  minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
+  drawHand(clockCtx, minute, radius * 0.8, radius * 0.07);
   // second
-  second = (second * Math.PI/30);
+  second = (second * Math.PI / 30);
   drawHand(clockCtx, second, radius * 0.9, radius * 0.02);
- 
+  clockCtx.fillStyle = 'red';
+  clockCtx.font = radius * 0.10 + "px arial";
+  clockCtx.fillText(`${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`,-50, -50);
+  requestAnimationFrame(drawAnimationPart);
 }
-
 
 function drawHand(ctx, pos, length, width) {
  
@@ -85,5 +87,5 @@ function drawHand(ctx, pos, length, width) {
 }
 
 createClock();
-setInterval(drawAnimationPart, 1000);
+drawAnimationPart();
 
